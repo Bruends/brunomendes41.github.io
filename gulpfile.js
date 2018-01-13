b03c3db264481b80beb4859533ae255c1f1599d8
Bruno Mendes <brunomendes962@gmail.com>
2018-01-13 10:50:50 -0500

    fixing css order in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,15 @@ const concatCss = require('gulp-concat-css');
 const autoprefixer = require('gulp-autoprefixer');
 const uglifyCss = require('gulp-uglifycss');
 
+const cssPaths = [
+  'style/animations.css',
+  'style/style.css',
+  'style/responsive.css'
+]
+
 // otimiza e adiciona prefixos no css
 gulp.task('default', () => {
-  return gulp.src('./style/*.css')
+  return gulp.src(cssPaths)
   .pipe(concatCss(''))
   .pipe(autoprefixer())
   .pipe(uglifyCss())
