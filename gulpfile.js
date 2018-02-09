ca3f39da7972077d630af4df95df952ba0941e79
Bruno Mendes <brunomendes962@gmail.com>
2018-02-09 13:21:37 -0200

    ajustes de layout e animação

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,23 @@ const gulp = require('gulp')
 const concatCss = require('gulp-concat-css');
 const autoprefixer = require('gulp-autoprefixer');
 const uglifyCss = require('gulp-uglifycss');
+var browserSync = require('browser-sync').create();
 
 const cssPaths = [
-  'style/animations.css',
   'style/style.css',
+  'style/animations.css',
   'style/responsive.css'
 ]
 
 // otimiza e adiciona prefixos no css
-gulp.task('default', () => {
+gulp.task('css', () => {
   return gulp.src(cssPaths)
   .pipe(concatCss(''))
   .pipe(autoprefixer())
   .pipe(uglifyCss())
   .pipe(gulp.dest('dist/style.min.css'));
+});
+
+gulp.task('default', () => {
+  gulp.watch('./style/*.css', ['css']);
 });
